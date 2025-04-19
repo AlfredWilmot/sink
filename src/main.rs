9615259d61843dfa05a081704537770229f80da1
AlfredWilmot <>
2025-04-19 23:30:14 +0100

    general refactor for deobfuscating masks

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{f32, process::exit};
 use clap::Parser;
 use colored::Colorize;
 
-/// Deconstruct a floating point number
+/// Coerce floating-points into fixed-point numbers.
 #[derive(Parser)]
 #[command(arg_required_else_help(true))]
 struct Args {
@@ -13,9 +13,9 @@ struct Args {
     number: f32,
 }
 
-///  bit-layout of the three components encoded into the f32 type:
+///  bit-pattern of the three components encoded into the f32 type:
 ///
-/// [] [<----- EXPONENT ---->] [<---- MANTISSA ---->]
+/// [] [<----- EXPONENT_MASK ---->] [<---- MANTISSA_MASK ---->]
 /// 31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 .. 0
 /// ^
 /// (sign)
@@ -23,31 +23,40 @@ struct Args {
 /// (assumes val is BigEndian)
 ///
 struct DeconstructedFloat32<'a> {
+    // reference to the original float this deconstruction is based on.
     float: &'a f32,
 
     // IEEE 754-XXXX standards define:
     //
-    sign_bit: u8,      // single bit stored in a byte
-    exponent_byte: u8, // byte is fully utilised
-    mantissa_bytes: [u8; 3], // 3 bytes with an unised bit (i.e. uses 23 bits)
-                       // RADIX = 2 (base)
-                       // BIAS = 127 (exponent offset)
+    // RADIX  = 2 (base)
+    // BIAS = 127 (exponent offset)
+    //
+    sign_bit: u8,
+    exponent_byte: u8,
+    mantissa_bytes: [u8; 3],
 }
 
 impl<'a> DeconstructedFloat32<'a> {
     /// create a deconstructed float from an input f32
     pub fn new(val: &'a f32) -> DeconstructedFloat32<'a> {
         // convert the input to u32 for bit-manipuation
-        let val_: u32 = val.to_bits();
+        let bits: u32 = val.to_bits();
 
-        // isolate the sign-bit
-        let sign_bit: u8 = (val_ >> 31) as u8;
+        // define some masks
+        const SIGN_MASK: u32 = 0b10000000_00000000_00000000_00000000; // sign-bit
+        const EXPO_MASK: u32 = 0b01111111_10000000_00000000_00000000; // exponent-byte
+        const MANT_MASK: u32 = 0b00000000_01111111_11111111_11111111; // mantissa-bytes
 
-        // isolate the exponent
-        let exponent_byte: u8 = (val_ >> 23) as u8;
-
-        // isolate mantissa bytes
-        let mantissa_bytes: [u8; 3] = DeconstructedFloat32::get_mantissa(val_);
+        // apply masks to bits and shift to extract relevant bytes for each component:
+        // (NOTE: masking sign_bit is redundant (it's the MSB) but done for consistency)
+        let sign_bit = ((bits & SIGN_MASK) >> 31) as u8;
+        let exponent_byte = ((bits & EXPO_MASK) >> 23) as u8;
+        let mantissa_bytes = [
+            (bits & MANT_MASK) >> 16,
+            (bits & MANT_MASK) >> 8,
+            (bits & MANT_MASK),
+        ]
+        .map(|v| v as u8);
 
         DeconstructedFloat32 {
             float: val,
@@ -57,15 +66,7 @@ impl<'a> DeconstructedFloat32<'a> {
         }
     }
 
-    /// Extract the mantissa bytes from a u32 representing a float.
-    fn get_mantissa(val: u32) -> [u8; 3] {
-        let byte_2 = val & 0b00000000_01111111_00000000_00000000; // 16..=22
-        let byte_1 = val & 0b00000000_00000000_11111111_00000000; // 8..=15
-        let byte_0 = val & 0b00000000_00000000_00000000_11111111; // 0..=7
-
-        [(byte_2 >> 16) as u8, (byte_1 >> 8) as u8, byte_0 as u8]
-    }
-
+    /// display the contents of the deconstructed float.
     fn print(&self) {
         let sign_bit_txt = format!("{:b}", self.sign_bit).on_red();
         let exponent_txt = format!("{:08b}", self.exponent_byte).on_red();
