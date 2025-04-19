ee91065cb9ae7dbd4e3ae8d505244b690c43c63f
AlfredWilmot <>
2025-04-19 17:10:31 +0100

    refactor, gather into DeconstructedFloat32 struct

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,14 @@
 #![allow(unused_variables, dead_code)]
 
 use clap::Parser;
+use colored::Colorize;
 
 /// Convert from a floating-point to fixed-point number
 #[derive(Parser)]
 #[command(arg_required_else_help(true))]
-struct  Args {
-
+struct Args {
     /// specify a floating point number
     number: f32,
-
-    /// indicate whether to print debug info
-    #[arg(short, long)]
-    debug: bool
-}
-
-/// Extract the sign-bit from a u32 representing a float.
-/// A Copy of val is bit-shifted so the sign-bit is the LSB
-/// (assumes val is BigEndian).
-fn get_sign(val: u32) -> u32 {
-    val >> 31
-}
-
-/// Extract the exponent-byte from a u32 representing a float.
-/// A Copy of val is bit-shifted so the exponent-byte is the LSB,
-/// and then a mask is used to remove the sign-bit
-/// that prepends the exponent-byte (assumes val is BigEndian).
-fn get_exponent(val: u32) -> u32 {
-    (val >> 23) & 0b_00000000_11111111
 }
 
 ///  bit-layout of the three components encoded into the f32 type:
@@ -37,26 +18,76 @@ fn get_exponent(val: u32) -> u32 {
 /// ^
 /// (sign)
 ///
+/// (assumes val is BigEndian)
+///
+struct DeconstructedFloat32<'a> {
+    float: &'a f32,
+
+    // IEEE 754-XXXX standards define:
+    //
+    sign_bit: u8,      // single bit stored in a byte
+    exponent_byte: u8, // byte is fully utilised
+    mantissa_bytes: [u8; 3], // 3 bytes with an unised bit (i.e. uses 23 bits)
+                       // RADIX = 2 (base)
+                       // BIAS = 127 (exponent offset)
+}
+
+impl<'a> DeconstructedFloat32<'a> {
+    /// create a deconstructed float from an input f32
+    pub fn new(val: &'a f32) -> DeconstructedFloat32<'a> {
+        // convert the input to u32 for bit-manipuation
+        let val_: u32 = val.to_bits();
+
+        // isolate the sign-bit
+        let sign_bit: u8 = (val_ >> 31) as u8;
+
+        // isolate the exponent
+        let exponent_byte: u8 = (val_ >> 23) as u8;
+
+        // isolate mantissa bytes
+        let mantissa_bytes: [u8; 3] = DeconstructedFloat32::get_mantissa(val_);
+
+        DeconstructedFloat32 {
+            float: &val,
+            sign_bit,
+            exponent_byte,
+            mantissa_bytes,
+        }
+    }
+
+    /// Extract the mantissa bytes from a u32 representing a float.
+    fn get_mantissa(val: u32) -> [u8; 3] {
+        let byte_2 = val & 0b00000000_01111111_00000000_00000000; // 16..=22
+        let byte_1 = val & 0b00000000_00000000_11111111_00000000; // 8..=15
+        let byte_0 = val & 0b00000000_00000000_00000000_11111111; // 0..=7
+
+        [
+            (byte_2 >> 16) as u8,
+            (byte_1 >> 8) as u8,
+            (byte_0 >> 0) as u8,
+        ]
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
     // get number from user input
     let float: f32 = args.number;
 
-    // convert the input to u32 for bit-manipuation
-    let val: u32 = float.to_bits();
+    // deconstructs float into its components
+    let float_ = DeconstructedFloat32::new(&float);
 
-    // isolate the sign-bit
-    let sign_bit: u32 = get_sign(val);
+    let sign_bit_txt = format!("{:b}", float_.sign_bit).on_red();
+    let exponent_txt = format!("{:08b}", float_.exponent_byte).on_red();
 
-    // isolate the Exponent
-    let exponent_byte: u32 = get_exponent(val);
+    let m_ = float_.mantissa_bytes;
+    let mantissa_txt = format!("{:07b}{:08b}{:08b}", m_[0], m_[1], m_[2]).on_red();
 
-    println!("{}", float);
-
-    if args.debug {
-        println!("| input    | {:032b} |", val);
-        println!("| sign     | {:032b} |", sign_bit);
-        println!("| exponent | {:032b} |", exponent_byte);
-    }
+    println!("\nInput: {}\n", float);
+    println!("| input (bits) | {:032b} |", float as u32);
+    println!("| sign         | {}{:031b} |", sign_bit_txt, 0);
+    println!("| exponent     | {:01b}{}{:023b} |", 0, exponent_txt, 0);
+    println!("| mantissa     | {:09b}{} |", 0, mantissa_txt);
+    println!("");
 }
