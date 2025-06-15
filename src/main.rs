ec6fbdcd5224f0923bd5e4223e35543196e6dc20
AlfredWilmot <18071042+AlfredWilmot@users.noreply.github.com>
2025-06-15 12:44:00 +0100

    wip: update cli with subcommands

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,63 @@
 use colored::Colorize;
 use std::{f32, process::exit};
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
-use sink::float::DeconstructedFloat32;
+use sink::{cpu::CPU, float::DeconstructedFloat32};
 
-/// Coerce floating-points into fixed-point numbers.
+/// Let's sink down into the dingy depths of the OS!
 #[derive(Parser)]
 #[command(arg_required_else_help(true))]
 struct Args {
-    /// specify a floating point number
-    number: f32,
+    #[command(subcommand)]
+    cmd: Commands,
 }
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Emulate a cpu that's loosely based on the CHIP-8 instruction set
+    Cpu{
+        /// pass a list of opcode instructions for the cpu to execute
+        opcodes: Vec<u16>,
+
+        /// load the cpu register with data
+        #[arg(short, long)]
+        register: Option<Vec<u8>>,
+
+    },
+    /// Deconstruct floats into their fixed-point binary representations
+    Float{
+        /// floating point number
+        number: f32
+    },
+}
+
 
 fn main() {
     let args = Args::parse();
 
-    // get number from user input
-    let float: f32 = args.number;
+    match args.cmd {
+        Commands::Float{number} => {
+            // is the number within the allowed range?
+            if (f32::MIN..=f32::MAX).contains(&number) {
+                DeconstructedFloat32::new(&number).print();
+                exit(0);
+            }
 
-    // is the number within the allowed range?
-    if (f32::MIN..=f32::MAX).contains(&float) {
-        DeconstructedFloat32::new(&float).print();
-        exit(0);
+            println!(
+                "{}",
+                format!("Must be within range: [{:?}, {:?}]", f32::MIN, f32::MAX).red(),
+            );
+        }
+        Commands::Cpu { opcodes, register } => {
+            let cpu = CPU::new();
+            // attempt to update the CPU register
+            if let Some(reg) = register {
+
+            }
+        }
+        _ => {},
     }
-
-    println!(
-        "{}",
-        format!("Must be within range: [{:?}, {:?}]", f32::MIN, f32::MAX).red(),
-    );
     exit(1);
+
 }
